028daf931c8521c3d8ca25a8c48bf5433c8a2858
Andrew Dunai <a@dun.ai>
2023-10-10 17:21:36 +0300

    Merge int 08h & int 1Ch from main

---
+++ rom/src/int1Ch.asm
@@ -0,0 +1,16 @@
+; =====================================================
+;
+; RTC handler stub (called by int 08h)
+;
+; This file is part of MetalBIOS for PK-88.
+;
+; =====================================================
+
+        cpu     8086
+        bits    16
+
+; BIOS 0x1C ISR
+global  int1Ch_isr
+int1Ch_isr:
+        ; no-op
+        iret
